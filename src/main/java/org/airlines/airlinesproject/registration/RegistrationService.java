575fcf41522743bbe1019981faeef22cb87e7f69
Jarosław Kopacz <ignacyikopacz@gmail.com>
2023-11-27 12:27:28 +0100

    build a registration service, and make userApp save method

--- src/main/java/org/airlines/airlinesproject/registration/RegistrationService.java
+++ src/main/java/org/airlines/airlinesproject/registration/RegistrationService.java
@@ -1,10 +1,33 @@
 package org.airlines.airlinesproject.registration;
 
+import lombok.AllArgsConstructor;
+import org.airlines.airlinesproject.appuser.AppUser;
+import org.airlines.airlinesproject.appuser.AppUserRole;
+import org.airlines.airlinesproject.appuser.AppUserService;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        final boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid: " + request.getEmail());
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        UUID.randomUUID(),
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
